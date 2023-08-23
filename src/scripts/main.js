886625d054307bdb4d2115e32ee888e7b868785b
anders iversen <ande397c@stud.kea.dk>
2023-08-23 18:28:28 +0200

    New active page logic

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -17,16 +17,16 @@ window.addEventListener("scroll", toggleNav);
      location.replace("/");
   })
 
-    // Add active class to current page
-    const currentLocation = location.href;
-    const menuItem = document.querySelectorAll(".nav_links a");
-    const menuLength = menuItem.length;
+  //   // Add active class to current page
+  //   const currentLocation = location.href;
+  //   const menuItem = document.querySelectorAll(".nav_links a");
+  //   const menuLength = menuItem.length;
 
-  for (let i = 0; i < menuLength; i++) {
-    if (menuItem[i].href === currentLocation) {
-      menuItem[i].className = "active-page";
-    }
-  }
+  // for (let i = 0; i < menuLength; i++) {
+  //   if (menuItem[i].href === currentLocation) {
+  //     menuItem[i].className = "active-page";
+  //   }
+  // }
 
     // Toggle burgermenu on click
     const burger = document.querySelector(".burger");
