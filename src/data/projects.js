7324cc2935c3b63c39beeee8e5cc9888cbdbb643
anders iversen <ande397c@stud.kea.dk>
2024-09-11 14:17:02 +0200

    Added projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,5 +1,5 @@
-//Twitter
-import twitterImage from '@assets/twitter-feed.png'
+// Project images:: 
+
 // Countries
 import countriesImage from '@assets/countries-overview.png'
 import countriesOverviewLight from '@assets/countries-overview-light.png'
@@ -19,10 +19,10 @@ export const projectData = [
     desc: 'Frontend Mentor project with light/dark theme. Made with vanilla JavaScript.',
     tag: 'countries',
     intro:
-      'This project was done in my spare time. The project itself is an advanced challenge on Frontend Mentor. The goal for me was to improve my JavaScript skills while incorporating the use of a REST API. In retrospect, I would have liked to include the use of a JavaScript framework in this project.',
+      'This project is an advanced challenge on Frontend Mentor. The goal for me was to improve my JavaScript skills while incorporating the use of an external API. The project supports dark/light theme. In retrospect, I would have liked to include the use of a JavaScript framework in this project.',
     demoLink: 'https://frontend-mentor-api-countries.netlify.app/',
     repoLink: 'https://github.com/ande397c/Frontend-Mentor-rest-countries-api',
-    stack: ['HTML', 'CSS', 'JavaScript', 'REST API'],
+    stack: ['HTML', 'CSS', 'JavaScript'],
     navigateLinks: {
       linkPrevious: '',
       linkNext: '/projects/rating-game/',
@@ -37,20 +37,57 @@ export const projectData = [
     slug: 'projects/rating-game/',
     heading: 'Movie rating game',
     projectImg: imdbImage,
-    desc: 'Guess which movie has the highest rating. Game made with React and TypeScript.',
+    desc: 'Guess which movie has the highest rating. Game made with React and TypeScript. Can you beat the current highscore?',
     tag: 'imdb',
     intro:
-      'This is a very personal project of mine. The idea for the project is a result of an early unbound fascination and interest for movies. As I started getting more into Web Development, I thought that it would make for a nice spare time project. The site uses Supabase for the backend.',
+      'The idea for the project sprung from an early unbound fascination and interest for movies. The goal for the user is to correctly guess the movie with highest rating. The user can submit the score and compare it against other users. The site uses Supabase for storing the data. Future plans for this project includes: Ability to choose categories, difficulties and more.',
     demoLink: 'https://rating-guess.netlify.app/',
     repoLink: 'https://github.com/ande397c/react-movie-rating/tree/main',
-    stack: [
-      'Tailwind',
-      'React',
-      'TypeScript',
-      'Supabase',
-    ],
+    stack: ['Tailwind', 'React', 'TypeScript', 'Supabase'],
     navigateLinks: {
       linkPrevious: '/projects/countries-project/',
+      linkNext: '/projects/weather-app/',
+    },
+    screenshots: {
+      img1: imdbIntro,
+      img2: imdbHighscores,
+      img3: imdbLost,
+    },
+  },
+  {
+    slug: 'projects/weather-app/',
+    heading: 'Weather app',
+    projectImg: imdbImage,
+    desc: 'Ios weather app clone. Made with React, Tailwind and Typescript',
+    tag: 'weather',
+    intro:
+      'This project is a clone of the ios weather app. The project used weather data from the OpenWeatherMap API to get both coordinates and actual Data. ',
+    demoLink: 'https://rating-guess.netlify.app/',
+    repoLink: 'https://github.com/ande397c/react-movie-rating/tree/main',
+    stack: ['Tailwind', 'React', 'TypeScript', 'REST API'],
+    navigateLinks: {
+      linkPrevious: '/projects/rating-game/',
+      linkNext: '/projects/kanban-board/',
+    },
+    screenshots: {
+      img1: imdbIntro,
+      img2: imdbHighscores,
+      img3: imdbLost,
+    },
+  },
+  {
+    slug: 'projects/kanban-board/',
+    heading: 'Kanban board',
+    projectImg: imdbImage,
+    desc: 'Kanban board, made with Vue3, Vuetify, Typescript and testet using Vitest.',
+    tag: 'kanban',
+    intro:
+      'This project was made to introduce myself to Vue and to corresponding technologies, like the ui library Vuetify. The board supports functionalities like, deleting and adding tasks and lists and dragging tasks. I am planning to hook the data up to a real backend to allow for data persistence.',
+    demoLink: 'https://vue-board-kanban.netlify.app/',
+    repoLink: 'https://github.com/ande397c/vue-kanban-board',
+    stack: ['Vue', 'Vuetify', 'TypeScript', 'Vitest'],
+    navigateLinks: {
+      linkPrevious: '/projects/weather-app/',
       linkNext: '',
     },
     screenshots: {
